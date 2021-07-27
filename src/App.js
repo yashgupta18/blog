92568241f8f284a029de5d57e5d2f672d80463fb
Yash Gupta <18guptayash@gmail.com>
2021-07-27 23:10:07 +0530

    Added Basic pages and navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home'
+import ArticlesList from './pages/ArticlesList'
+import Article from './pages/Article'
+import About from './pages/About'
+import Navbar from './components/Navbar'
 
+
+import {BrowserRouter as Router, Route} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <Navbar />
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/article" component={Article} />
+        <Route exact path="/articles-list" component={ArticlesList} />
+        <Route exact path="/about" component={About} />
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="mb-20">
+            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900"> Welcome to my blog</h1>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea ab soluta magni iste nisi neque quibusdam. At asperiores, quod similique deserunt veniam tempora hic tempore sequi nemo dolores illum quibusdam.
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aperiam nam accusantium aut facere voluptatum mollitia quod tempore optio, adipisci et architecto est perspiciatis expedita commodi necessitatibus, odit veritatis. Voluptatibus!
+            </p>
+            <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem dolorum praesentium necessitatibus eum eos accusamus.</p>
+        </div>
+    )
+}
+
+export default Home
