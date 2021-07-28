edda2ed02529bb214e6b18ce86b92f993456a462
Yash Gupta <18guptayash@gmail.com>
2021-07-28 23:10:12 +0530

    Crreate article component and link to different articles

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import articleContent from "./article-content"
+import React from "react";
+import Articles from "../components/Articles";
+import articleContent from "./article-content";
 const Article = ({ match }) => {
-	console.log(match);
-	const name = match.params.name;
-	const article = articleContent.find((article) => article.name === name);
-	if(!article) return <h1>Article does not exist</h1>
-    return (
-        <>
-					<h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-						{article.title}
-				</h1>
-				{article.content.map((paragraph, index) => (
-					<p className="mx-auto loading-relaxed text-base mb-4" key={index}>
-						{paragraph}
-					</p>
-				))}
-        </>
-    )
-}
+  console.log(match);
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
+  if (!article) return <h1>Article does not exist</h1>;
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+  return (
+    <>
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+        {article.title}
+      </h1>
+      {article.content.map((paragraph, index) => (
+        <p className="mx-auto loading-relaxed text-base mb-4" key={index}>
+          {paragraph}
+        </p>
+      ))}
+      <h1 className="sm:text-2x text-xl font-bold mt-4 mb-4 text-gray-900">
+        Other Articles
+      </h1>
+      <div className="flex flex-wrap -m-4">
+        <Articles articles={otherArticles} />
+      </div>
+    </>
+  );
+};
 
-export default Article
+export default Article;
--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
+import Articles from "../components/Articles";
+import articleContent from "./article-content";
 
 const ArticlesList = () => {
-    return (
-        <>
-            <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-                Articles List
-            </h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
+        Articles List
+      </h1>
+      <div className="container py-4 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <Articles articles={articleContent} />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ArticlesList
+export default ArticlesList;
